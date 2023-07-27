6e7541083a5a393295613c6ce6fdc039d9c2b2b4
Serhii151 <122047001+Serhii151@users.noreply.github.com>
2023-07-27 23:56:52 +0300

    Updt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { AppContainer } from './App.styles';
+import { saveContactsToLocalStorage, getContactsFromLocalStorage } from './LocalStorageFunctions/ContactStorage';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
+  const initialContacts = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
+
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = getContactsFromLocalStorage();
+    return storedContacts.length > 0 ? storedContacts : initialContacts;
+  });
+
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    saveContactsToLocalStorage(contacts);
+  }, [contacts]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
